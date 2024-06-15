9007152b38aeba7b0f043332c3fc1e9f825cc5f5
Alejandro260807 <acrdonamarulanda@gmail.com>
2024-06-15 09:17:47 -0500

    hola

--- scripts.js
+++ scripts.js
@@ -29,7 +29,7 @@ function dividir() {
 function potencia() {
   var num1 = parseFloat(document.getElementById('num1').value);
   var num2 = parseFloat(document.getElementById('num2').value);
-  var resultado = Math.pow(num1, num2);num1 / num2;
+  var resultado = num1 / num2;
   document.getElementById('resultado').innerText = resultado;
 }
 
@@ -38,17 +38,4 @@ function raiz() {
   var num1 = parseFloat(document.getElementById('num1').value);
   var resultado = Math.sqrt(num1);
   document.getElementById('resultado').innerText = resultado;
-}
-
-function modulo() {
-  var num1 = parseFloat(document.getElementById('num1').value);
-  var num2 = parseFloat(document.getElementById('num2').value);
-  var resultado = num1 % num2;
-  document.getElementById('resultado').innerText = resultado;
-}
-
-function cleaner(){
-  document.getElementById('num1').value="";
-  document.getElementById('num2').value="";
-  document.getElementById('resultado').innerHTML="";
 }
